2771285792bfe045277474ff97a7afe100f1bb53
Ilya Malgin <Soundmaker1505@gmail.com>
2024-02-02 08:00:27 +0300

    pointers header

---
+++ Engine/include/pointers.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <memory>
+
+namespace Storyteller
+{
+    template<typename T>
+    using Ptr = std::shared_ptr<T>;
+
+    template<typename T, typename... Args>
+    constexpr Ptr<T> CreatePtr(Args&&... args)
+    {
+        return std::make_shared<T>(std::forward<Args>(args)...);
+    }
+
+    template<typename T>
+    using UPtr = std::unique_ptr<T>;
+
+    template<typename T, typename... Args>
+    constexpr UPtr<T> CreateUPtr(Args&&... args)
+    {
+        return std::make_unique<T>(std::forward<Args>(args)...);
+    }
+}
