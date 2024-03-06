e9eabb0e239784045d85e48883dc417d75cea8fa
Ilya Malgin <Soundmaker1505@gmail.com>
2024-03-06 18:27:41 +0300

    translate context functions, .po update script fix

--- Engine/include/Storyteller/localization_manager.h
+++ Engine/include/Storyteller/localization_manager.h
@@ -20,6 +20,8 @@ namespace Storyteller
         void AddMessagesDomain(const std::string& domain);
         std::string Translate(const std::string& domain, const std::string& message);
         std::string Translate(const std::string& domain, const std::string& messageSingular, const std::string& messagePlural, int count);
+        std::string TranslateCtx(const std::string& domain, const std::string& message, const std::string& context);
+        std::string TranslateCtx(const std::string& domain, const std::string& messageSingular, const std::string& messagePlural, int count, const std::string& context);
         bool CreateTranslations(const Ptr<GameDocument> document, const std::filesystem::path& path) const;
 
     private:
--- Engine/src/localization_manager.cpp
+++ Engine/src/localization_manager.cpp
@@ -52,7 +52,19 @@ namespace Storyteller
 
     std::string LocalizationManager::Translate(const std::string& domain, const std::string& messageSingular, const std::string& messagePlural, int count)
     {
-        return (boost::locale::format(boost::locale::translate(messageSingular, messagePlural, count)) % count).str();
+        return (boost::locale::format(boost::locale::translate(messageSingular, messagePlural, count).str(domain)) % count).str();
+    }
+    //--------------------------------------------------------------------------
+
+    std::string LocalizationManager::TranslateCtx(const std::string& domain, const std::string& message, const std::string& context)
+    {
+        return boost::locale::translate(context, message).str(domain);
+    }
+    //--------------------------------------------------------------------------
+
+    std::string LocalizationManager::TranslateCtx(const std::string& domain, const std::string& messageSingular, const std::string& messagePlural, int count, const std::string& context)
+    {
+        return (boost::locale::format(boost::locale::translate(context, messageSingular, messagePlural, count).str(domain)) % count).str();
     }
     //--------------------------------------------------------------------------
 
