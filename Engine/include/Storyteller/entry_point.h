fb664c0a8b571a2e0ab65e797344ecd649400f92
Ilya Malgin <Soundmaker1505@gmail.com>
2024-03-25 18:06:43 +0300

    WinMain entry point, conditional in cmake

--- Engine/include/Storyteller/entry_point.h
+++ Engine/include/Storyteller/entry_point.h
@@ -1,7 +1,27 @@
 #pragma once
 
 #include "application.h"
+#include "platform.h"
 
+#if defined STRTLR_WINMAIN
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
+{
+    Storyteller::Application* app = Storyteller::CreateApplication();
+    if (!app)
+    {
+        return -1;
+    }
+
+    app->Initialize();
+    app->Run();
+
+    delete app;
+
+    return 0;
+}
+//--------------------------------------------------------------------------
+
+#else
 int main(int argc, char** argv)
 {
     Storyteller::Application* app = Storyteller::CreateApplication();
@@ -17,4 +37,5 @@ int main(int argc, char** argv)
 
     return 0;
 }
+#endif
 //--------------------------------------------------------------------------
