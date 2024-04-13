a9cc73fb2ff63a8dc9d116498d89fb17d9bcc6c8
Ilya Malgin <Soundmaker1505@gmail.com>
2024-04-13 17:33:02 +0300

    kbhit linux

--- Engine/src/console_utils.cpp
+++ Engine/src/console_utils.cpp
@@ -4,9 +4,7 @@
 #if defined STRTLR_PLATFORM_WINDOWS
 #include <conio.h>
 #elif defined STRTLR_PLATFORM_LINUX
-#include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 #endif
@@ -20,22 +18,37 @@ namespace Storyteller
 #if defined STRTLR_PLATFORM_WINDOWS
             return _kbhit();
 #else
-            int oldf, ch;
-
-            oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-            fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-            ch = getchar();
-
-            fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-            if (ch != EOF)
+            char buf = 0;
+            struct termios old = { 0 };
+            if (tcgetattr(0, &old) < 0)
             {
-                ungetc(ch, stdin);
-                return 1;
+                perror("tcsetattr()");
             }
 
-            return 0;
+            old.c_lflag &= ~ICANON;
+            old.c_lflag &= ~ECHO;
+            old.c_cc[VMIN] = 1;
+            old.c_cc[VTIME] = 0;
+
+            if (tcsetattr(0, TCSANOW, &old) < 0)
+            {
+                perror("tcsetattr ICANON");
+            }
+
+            if (read(0, &buf, 1) < 0)
+            {
+                perror("read()");
+            }
+
+            old.c_lflag |= ICANON;
+            old.c_lflag |= ECHO;
+
+            if (tcsetattr(0, TCSADRAIN, &old) < 0)
+            {
+                perror("tcsetattr ~ICANON");
+            }
+
+            return (buf);
 #endif
         }
         //--------------------------------------------------------------------------
