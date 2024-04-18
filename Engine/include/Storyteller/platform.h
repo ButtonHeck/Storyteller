ae08e56250a00a92d0f5e442c16ebdd294785f4d
Ilya Malgin <Soundmaker1505@gmail.com>
2024-04-18 18:12:46 +0300

    NOMINMAX msvc fix

--- Engine/include/Storyteller/platform.h
+++ Engine/include/Storyteller/platform.h
@@ -1,21 +1,25 @@
 #pragma once
 
-#if defined (_WIN32) || defined (_WIN64)
-    #define STRTLR_PLATFORM_WINDOWS
-    #define NOGDI
-    #define NOMINMAX
-    #define WIN32_LEAN_AND_MEAN
-    #include <windows.h>
-#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
-    #define STRTLR_PLATFORM_LINUX
-#elif define (__APPLE__)
-    #define STRTLR_PLATFORM_MACOSX
-#endif
-
 #if defined (_MSC_VER)
     #define STRTLR_COMPILER_MSVC
 #elif defined (__MINGW32__) || defined (__MINGW64__)
     #define STRTLR_COMPILER_MINGW
 #elif defined (__GNUC__)
     #define STRTLR_COMPILER_GCC
+#elif defined(__clang__)
+    #define STRTLR_COMPILER_CLANG
+#endif
+
+#if defined (_WIN32) || defined (_WIN64)
+    #define STRTLR_PLATFORM_WINDOWS
+    #define NOGDI
+    #if defined (STRTLR_COMPILER_MSVC)
+        #define NOMINMAX
+    #endif
+    #define WIN32_LEAN_AND_MEAN
+    #include <windows.h>
+#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    #define STRTLR_PLATFORM_LINUX
+#elif define (__APPLE__)
+    #define STRTLR_PLATFORM_MACOSX
 #endif
